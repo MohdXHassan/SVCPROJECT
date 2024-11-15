a3b96907d858f4372ec9b0a764cb56efb71aaa4a
ZARIX <46120048+MohdXHassan@users.noreply.github.com>
2024-11-15 22:35:50 +0530

    Update server.c
    header file

--- server.c
+++ server.c
@@ -1,1 +1,9 @@
+#include <stdio.h> // std input output 
+#include <stdlib.h> // std library
+#include <string.h> // for strings 
+#include <unistd.h>    // for miscellaneous functions
+#include <sys/types.h> // for types TCP / UDP
+#include <sys/socket.h> // for sockets
+#include <netinet/in.h> // for Address family 
+
 
